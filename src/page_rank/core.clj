fa73aefcf1ba6102a89219c94ef007e273373573
Juan Facorro <juan.facorro@gmail.com>
2014-10-11 13:02:58 -0300

    Implementation was working, matrixes were wrong.

--- src/page_rank/core.clj
+++ src/page_rank/core.clj
@@ -3,58 +3,58 @@
   (:use clojure.core.matrix)
   (:use clojure.core.matrix.operators))
 
-(def max-iterations 5)
-(def sqrt-2 (Math/sqrt 2))
+(def max-iterations 1000)
 
 (defn- sum-1-beta [beta r]
-  (map 
-    (partial + (/ (- 1 beta) 
-                 (count r)))
-    r))
+  (let [delta  (/ (- 1 beta) (count r))]
+    (map (partial + delta) r)))
 
 (defn normalise-prob [r]
-  (->> r
-    normalise
-    (map #(/ % sqrt-2))))
+  (let [delta (/ (- 1 (reduce + r))
+                 (count r))]
+    (map (partial + delta) r)))
 
 (defn iteration
   [r M beta]
-  (->> (mmul r M) 
+  (->> (mmul M r)
     (* beta)
-    normalise-prob
-    (sum-1-beta beta)))
+    (sum-1-beta beta)
+    #_normalise-prob))
 
 (defn power-iteration
-  [r M beta epsilon]
+  [r M beta epsilon & [n]]
   (loop [i 0
-         r r]
-    (let [r-new (iteration r M beta)
-          diff  (distance r r-new)]
-      (prn i diff)
-      (if (or (< max-iterations i) (< diff epsilon))
+         r-old r]
+    (let [r-new (iteration r-old M beta)
+          diff  (distance r-old r-new)]
+      (prn :iter (inc i) :new r-new)
+      (if (or (<= (or n max-iterations) (inc i))
+              (< diff epsilon))
         r-new
         (recur (inc i) r-new)))))
 
-(comment
+(comment)
   ;; 1
-  (def M [[0 1 1/3]
-          [0 0 1/3]
-          [0 0 1/3]])
-  (def r [1 1 1])
-  (let [result  (power-iteration r M 0.7 0.0000001)
-        [a b c] (->> result
-                  normalise-prob
-                  (map (partial * 3)))]
+  (let [beta    0.7
+        epsilon 0.0000001
+        M       [[0 0 0]
+                 [1/2 0 0]
+                 [1/2 1 1]]
+        r       [1/3 1/3 1/3]
+        N       (vec (repeat 3 r))
+        result  (power-iteration r M beta epsilon)
+        [a b c] (->> result (map (partial * 3)))]
     (println (reduce + [a b c]))
     (println :a+c (+ a c))
     (println :a+b (+ a b))
     (println :b+c (+ b c)))
-)
+
 
   ;; 3
-  (def M [[0 1 1/2]
-          [0 0 1/2]
-          [1 0 0]])
-  (def r [1 1 1])
-  (let [result  (power-iteration r M 1 0.000000001)]
-    (println result))
+  (let [M       [[0   0 1]
+                 [1/2 0 0]
+                 [1/2 1 0]]
+        r       [1 1 1]
+        result  (power-iteration r M 1 0.000001 6)]
+    (println result)
+    (println (map float result)))
